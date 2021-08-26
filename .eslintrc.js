60feb9aa640945adeb63fe7239ab95aff5c74e8f
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2021-08-26 12:32:11 +0200

    Config for eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript/base',
+    'prettier',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+    project: './tsconfig.json',
+    tsconfigRootDir: './',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {},
+  ignorePatterns: ['.eslintrc.js'],
+};
