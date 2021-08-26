86220f3ba99687fbf1ee5d0d22c81e889a53c826
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2021-08-26 15:08:46 +0200

    Linted with new rule after moving plugins to plugins in eslintrc.js

--- src/controllers/wilder.ts
+++ src/controllers/wilder.ts
@@ -23,8 +23,9 @@ const wilderController = {
   update: (req: Request, res: Response): void => {
     WilderModel.updateOne({ _id: req.body._id }, req.body)
       .then((result) => {
-        if (!result)
+        if (!result) {
           res.json({ success: false, result: 'No such wilder exists' });
+        }
 
         res.json(result);
       })
@@ -35,11 +36,12 @@ const wilderController = {
   delete: (req: Request, res: Response): void => {
     WilderModel.deleteOne({ _id: req.body._id })
       .then((result) => {
-        if (!result)
+        if (!result) {
           res.json({
             success: false,
             result: 'No wilder with such ID was found',
           });
+        }
         res.json({ success: true, result });
       })
       .catch((err) => res.json({ success: false, result: err }));
